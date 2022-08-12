80046232847ae52d0d7102ad0d65cd778dd3accd
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-11 23:03:06 -0400

    commit tree e7f30e6a7b6e991c7ee4b95d6253cfc5807f9931

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop(2);
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+  }
+
+  function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    return newCats;
+  }
+
+  function prependCat(name) {
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+  }
+
+  function removeLastCat() {
+    const newCats = cats.slice(0,2);
+    return newCats;
+  }
+
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
